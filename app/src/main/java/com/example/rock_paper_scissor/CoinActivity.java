328cdf3b3d94b569c0e6378250ec81346615da23
JoseJaraba <jose.jaraba@upb.edu.co>
2021-04-16 16:35:27 -0500

    all views added

---
+++ app/src/main/java/com/example/rock_paper_scissor/CoinActivity.java
@@ -0,0 +1,14 @@
+package com.example.rock_paper_scissor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin);
+    }
+}
--- app/src/main/java/com/example/rock_paper_scissor/MainActivity.java
+++ app/src/main/java/com/example/rock_paper_scissor/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.rock_paper_scissor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
